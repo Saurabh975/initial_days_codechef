70bb9cd4ab8df0d8306a203d3b32d031045c0c73
Saurabh975 <sktsaurabh975@gmail.com>
2020-04-20 19:11:00 +0530

    Initial commit

---
+++ CodeChef/src/buddyNim.java
@@ -0,0 +1,288 @@
+
+// Author @ BlackRise :) //
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class buddyNim {
+    static Calendar ts, te;                                                  //For time calculation
+    static long mod9 = (long) 1e9 + 7;
+    static InputReader input = new InputReader(System.in);
+    static PrintWriter pw = new PrintWriter(System.out, true);
+
+
+    public static void main(String[] args) {                                 //threading has been used to increase the stack size.
+
+        new Thread(null, null, "BlackRise", 1 << 25)  //the last parameter is stack size which is desired,
+        {
+            public void run() {
+
+                try {
+                    Blackrise();
+                    System.gc();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            }
+        }.start();
+    }
+
+    static void Blackrise() {                                                //The name Blackrise is my pen name... you can change the name according to your wish
+
+        tsc();                                                               //calculates the starting time of execution
+
+        int t = ni();
+        while (t-- > 0) {
+            int n = ni();
+            int m = ni();
+//            long a[] = new long[n];
+//            long b[] = new long[m];
+
+            ArrayList<Integer> a = new ArrayList<>();
+            ArrayList<Integer> b = new ArrayList<>();
+            long s1 = 0, s2 = 0;
+
+            for (int i = 0; i < n; i++) {
+                int x = ni();
+                if (x != 0) {
+                    a.add(x);
+                    s1 += x;
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                int x = ni();
+                if (x != 0) {
+                    b.add(x);
+                    s2 += x;
+                }
+            }
+            Collections.sort(a);
+            Collections.sort(b);
+            boolean temp=a.size()==b.size();
+            if(temp)
+            {
+                for(int i=0;i<a.size();i++)
+                    if(!a.get(i).equals( b.get(i))) {
+                        temp = false;
+                        break;
+                    }
+            }
+            //else temp=1;
+
+            pl(temp?"Bob":"Alice");
+        }
+
+        tec();                  //calculates the ending time of execution
+        //pwt();                //prints the time taken to execute the program
+    }
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    static class InputReader {                                              //InputReader class for fast input
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine() {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        public int nextInt() {
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    // functions to take input//
+    static int ni() {
+        return input.nextInt();
+    }
+
+    static long nl() {
+        return input.nextLong();
+    }
+
+    static double nd() {
+        return input.nextDouble();
+    }
+
+    static String ns() {
+        return input.readString();
+    }
+
+    //functions to give output
+    static void pl() {
+        pw.println();
+    }
+
+    static void p(Object o) {
+        pw.print(o + " ");
+    }
+
+    static void pws(Object o) {
+        pw.print(o + "");
+    }
+
+    static void pl(Object o) {
+        pw.println(o);
+    }
+    //time calculation functions
+
+    //starting time
+    static void tsc() {
+        ts = Calendar.getInstance();
+        ts.setTime(new Date());
+    }
+
+    //ending time
+    static void tec() {
+        te = Calendar.getInstance();
+        te.setTime(new Date());
+    }
+
+    static void pwt() {
+        pw.printf("\nExecution time was :- %f s\n", (te.getTimeInMillis() - ts.getTimeInMillis()) / 1000.00);
+    }
+}
